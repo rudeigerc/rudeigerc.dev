95a55da8007a9b1de7bb521629813ab3d099be4b
rudeigerc <rudeigerc@gmail.com>
2024-09-22 11:40:48 +0800

    feat: add gtag support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
@@ -18,6 +20,11 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [remarkMath],
