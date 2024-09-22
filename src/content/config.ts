e4832f690aa221eb02318ccd4ac4a42b94bdc31c
rudeigerc <rudeigerc@gmail.com>
2024-09-22 20:03:05 +0800

    feat: support draft posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,19 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    draft: z.boolean().optional().default(false),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    series: z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = { blog };
