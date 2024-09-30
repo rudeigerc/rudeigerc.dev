3e0ef91c24d53b24bca070fee6e43cbe329c208b
rudeigerc <rudeigerc@gmail.com>
2024-09-30 19:48:12 +0800

    chore: update code block line numbers related styles and remove extra blank line when rendering code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,12 @@ import tailwind from "@astrojs/tailwind";
 import { rehypeShiki } from "@astrojs/markdown-remark";
 import rehypeKatex from "rehype-katex";
 import rehypeMermaid from "rehype-mermaid";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkMath from "remark-math";
 import remarkGithubAlerts from "remark-github-alerts";
 import { transformerTitle } from "@rudeigerc/shiki-transformer-title";
+import { transformerMetaHighlight } from "@shikijs/transformers";
 
 // https://astro.build/config
 export default defineConfig({
@@ -39,9 +42,20 @@ export default defineConfig({
         rehypeShiki,
         {
           theme: "github-dark",
-          transformers: [transformerTitle()],
+          transformers: [
+            {
+              // https://github.com/shikijs/shiki/issues/3#issuecomment-2272168959
+              preprocess(code) {
+                return code.endsWith("\n") ? code.slice(0, -1) : code;
+              },
+            },
+            transformerTitle(),
+            transformerMetaHighlight(),
+          ],
         },
       ],
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "append" }],
     ],
     syntaxHighlight: false,
   },
