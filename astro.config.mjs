48213e4746aa1dd6655c05882e9c1af7af2d56ed
rudeigerc <rudeigerc@gmail.com>
2024-10-07 12:27:20 +0800

    feat: support anchor of headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import remarkMath from "remark-math";
 import remarkGithubAlerts from "remark-github-alerts";
 import { transformerTitle } from "@rudeigerc/shiki-transformer-title";
 import { transformerMetaHighlight } from "@shikijs/transformers";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
 
 // https://astro.build/config
 export default defineConfig({
@@ -53,7 +54,19 @@ export default defineConfig({
         },
       ],
       rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "append" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          content: /** @type {Array<ElementContent>} */ (
+            fromHtmlIsomorphic(
+              '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hash"><line x1="4" x2="20" y1="9" y2="9"/><line x1="4" x2="20" y1="15" y2="15"/><line x1="10" x2="8" y1="3" y2="21"/><line x1="16" x2="14" y1="3" y2="21"/></svg>',
+              { fragment: true },
+            ).children
+          ),
+          behavior: "append",
+          properties: { ariaHidden: true, tabIndex: -1, className: "anchor" },
+        },
+      ],
     ],
     syntaxHighlight: false,
   },
