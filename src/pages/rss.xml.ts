bdde2a1ad8e0d16af084de158b1cd7732e5ac0dd
rudeigerc <rudeigerc@gmail.com>
2025-02-26 17:56:01 +0800

    feat: support full post content in rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,10 @@ import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE } from "@/consts";
 import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async (context) => {
   const posts = await getCollection("blog");
@@ -11,8 +15,12 @@ export const GET: APIRoute = async (context) => {
     description: SITE_DESCRIPTION,
     site: context.site ?? "",
     items: posts.map((post) => ({
-      ...post.data,
       link: `/posts/${post.id}/`,
+      content: sanitizeHtml(parser.render(post.body ?? ""), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+      ...post.data,
     })),
+    customData: "<language>zh-Hans</language>",
   });
 };
