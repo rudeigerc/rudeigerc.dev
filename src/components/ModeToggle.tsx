0b11032595037e1a76cc17a142bf6deb332309bf
rudeigerc <rudeigerc@gmail.com>
2025-08-20 22:50:21 +0800

    feat: support dark mode toggle
    Signed-off-by: rudeigerc <rudeigerc@gmail.com>

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,5 +1,5 @@
-import { Moon, Sun } from "lucide-react";
 import * as React from "react";
+import { Moon, Sun } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -30,7 +30,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="ghost" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
